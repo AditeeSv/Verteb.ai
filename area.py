be93bad67fb7e64ca767c84685e398e0746046a0
musk4n <muskany19@gmail.com>
2021-11-12 17:34:41 +0530

    area calc

---
+++ area.py
@@ -0,0 +1,40 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+area=[]
+X=[]
+i=0
+while True:
+    # Read the frame
+    _, img = cap.read()
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        # print("CORDINATES:", x,y,x+w,y+h)
+        # print("area:", w*h)
+        area.append(w*h)
+        X.append(i)
+        i+=1
+    # Display
+    cv2.imshow('img', img)
+    # Stop if escape key is pressedG
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+# Release the VideoCapture object
+cap.release()
+
+plt.plot(X, area)
+plt.show()
